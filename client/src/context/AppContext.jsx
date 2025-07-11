3f5b33ce228608fd0446674448eac8350b6939c7
siddhit-kale <ksiddhit9@gmail.com>
2025-07-11 18:47:39 +0530

    Image model & route

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
-// import { toast } from "react-toastify";
-// import axios from 'axios'
-// import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
@@ -9,68 +9,67 @@ const AppContextProvider = (props) => {
 
     const [showLogin, setShowLogin] = useState(false);
     
-    // const [token, setToken] = useState(localStorage.getItem('token'))
+    const [token, setToken] = useState(localStorage.getItem('token'))
 
-    // const [credit, setCredit] = useState(false)
+    const [credit, setCredit] = useState(false)
 
-    // const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
 
-    // const isAuthenticated = !!token;
+    const isAuthenticated = !!token;
 
-    // const loadCreditsData = async () => {
-    //     try {
-    //         const { data } = await axios.get(backendUrl + '/api/user/credits', { headers: { token } })
+    const loadCreditsData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/user/credits', { headers: { token } })
 
-    //         if (data.success) {
-    //             setCredit(data.credits)
-    //             setUser(data.user)
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //         toast.error(error.message)
-    //     }
-    // }
-
-
-    // const generateImage = async (prompt) => {
-    //     try {
-    //         const { data } = await axios.post(backendUrl + '/api/image/generate-image', { prompt }, { headers: { token } })
-
-    //         if (data.success) {
-    //             loadCreditsData()
-    //             return data.resultImage
-    //         } else {
-    //             toast.error(data.message)
-    //             loadCreditsData()
-    //             if (data.creditBalance === 0) {
-    //                 navigate('/buy')
-    //             }
-    //         }
-    //     } catch (error) {
-    //         toast.error(error.message)
-    //     }
-    // }
-
-    // const logout = () => {
-    //     localStorage.removeItem('token')
-    //     setToken('')
-    //     setUser(null)
-    //     navigate('/')
+            if (data.success) {
+                setCredit(data.credits)
+                setUser(data.user)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
 
 
-    // }
+    const generateImage = async (prompt) => {
+        try {
+            const { data } = await axios.post(backendUrl + '/api/image/generate-image', { prompt }, { headers: { token } })
 
-    // useEffect(() => {
-    //     if (token) {
-    //         loadCreditsData()
-    //     }
-    // }, [token])
+            if (data.success) {
+                loadCreditsData()
+                return data.resultImage
+            } else {
+                toast.error(data.message)
+                loadCreditsData()
+                if (data.creditBalance === 0) {
+                    navigate('/buy')
+                }
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        setToken('')
+        setUser(null)
+        navigate('/')
+
+
+    }
+
+    useEffect(() => {
+        if (token) {
+            loadCreditsData()
+        }
+    }, [token])
 
     const value = {
-        user, setUser, showLogin, setShowLogin,
-            //  backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout, generateImage, isAuthenticated
+        user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout, generateImage, isAuthenticated
     }
 
     return (
